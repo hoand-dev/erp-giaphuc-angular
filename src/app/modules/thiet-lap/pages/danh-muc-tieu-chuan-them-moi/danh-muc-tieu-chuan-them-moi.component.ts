e0ef073ea9cb0e2cfc10fa1c72f9c096d7e674e6
hoand-dev <hoand.dev@gmail.com>
2020-10-22 16:58:34 +0700

    add component danh muc tieu chuan them moi

---
+++ src/app/modules/thiet-lap/pages/danh-muc-tieu-chuan-them-moi/danh-muc-tieu-chuan-them-moi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-danh-muc-tieu-chuan-them-moi',
+  templateUrl: './danh-muc-tieu-chuan-them-moi.component.html',
+  styleUrls: ['./danh-muc-tieu-chuan-them-moi.component.css']
+})
+export class DanhMucTieuChuanThemMoiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
