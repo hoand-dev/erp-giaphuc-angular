987f136da633c47580db7119b9334de840497ce1
hoand-dev <hoand.dev@gmail.com>
2020-10-24 08:08:56 +0700

    change findChiNhanhs auth and not auth

--- src/app/shared/services/thiet-lap/chi-nhanh.service.ts
+++ src/app/shared/services/thiet-lap/chi-nhanh.service.ts
@@ -1,10 +1,11 @@
 import { BaseService } from '@app/shared/services';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ChiNhanh } from '@app/shared/entities';
 
 import { environment } from '@environments/environment';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
@@ -19,7 +20,11 @@ export class ChiNhanhService extends BaseService {
         return this.httpClient.get<ChiNhanh>(this.apiUrl + `/${id}`);
     }
 
-    findChiNhanhs(): Observable<ChiNhanh[]> {
+    findChiNhanhs(notAuth: boolean = false): Observable<ChiNhanh[]> {
+        if (notAuth){
+            var reqHeader = new HttpHeaders({ 'Content-Type': 'application/x-www-urlencoded', 'No-Auth': 'True' });
+            return this.httpClient.get<any>(this.apiUrl + '/not-auth', { headers: reqHeader });
+        }
         return this.httpClient.get<ChiNhanh[]>(this.apiUrl);
     }
 
