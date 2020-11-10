bd639dfdd0f513cd1a3a85216087b87e38a0c306
LarryTeo <44776310+LarryTeo@users.noreply.github.com>
2020-11-10 16:46:11 +0700

    Add NhomNhaCungCap-NhomKhachHang

--- src/app/modules/thiet-lap/pages/nhom-khach-hang-them-moi/nhom-khach-hang-them-moi.component.ts
+++ src/app/modules/thiet-lap/pages/nhom-khach-hang-them-moi/nhom-khach-hang-them-moi.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { DxFormComponent } from 'devextreme-angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { NhomKhachHang } from '@app/shared/entities';
+import { NhomKhachHangService } from '@app/shared/services';
+import notify from 'devextreme/ui/notify';
 
 @Component({
   selector: 'app-nhom-khach-hang-them-moi',
@@ -7,9 +14,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NhomKhachHangThemMoiComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(DxFormComponent, {static: false}) frmNhomKhachHang: DxFormComponent;
+
+  private subscription: Subscription;
+  public  nhomkhachhang: NhomKhachHang;
+
+  public  saveProcessing = false;
+
+  public  rules: Object = { 'X': /[02-9]/};
+  public buttonSubmitOptions: any = {
+    text: "Lưu lại",
+    type: "success",
+    useSubmitBehavior: true
+  } 
+
+  constructor(
+    private router: Router,
+    private  activatedRoute: ActivatedRoute,
+    private nhomkhachhangService: NhomKhachHangService
+  ) { }
+
+  ngAfterViewInit(){
+
+  }
 
   ngOnInit(): void {
+    this.nhomkhachhang = new NhomKhachHang();
+    this.theCallbackValid = this.theCallbackValid.bind(this);
+  }
+  ngOnDestroy(): void {
+    if(this.subscription){
+      this.subscription.unsubscribe();
+    }
+  }
+
+  theCallbackValid(params){
+    return  this.nhomkhachhangService.checkExistNhomkhachHang(params.value);
+
+  }
+
+  onSubmitForm(e){
+    let nhomkhachhang_req = this.nhomkhachhang;
+
+    this.saveProcessing =true;
+    this.subscription = this.nhomkhachhangService.addNhomKhachHang(nhomkhachhang_req).subscribe(
+      data => {
+        notify({
+          width: 320,
+          message: "Lưu thành công",
+          position: { my: "right top", at: "right top"}
+        }, "sucess", 475);
+        this.router.navigate(['/nhom-khach-hang']); // chuyển trang sau khi thêm
+        this.frmNhomKhachHang.instance.resetValues();
+        this.saveProcessing = false;
+      },
+      error => {
+        this.nhomkhachhangService.handleError(error);
+        this.saveProcessing = false;
+      }
+    );
+    e.preventDefault();
   }
 
 }
---
+++ src/app/shared/services/thiet-lap/nhom-nha-cung-cap.service.ts
@@ -0,0 +1,60 @@
+import { Observable } from 'rxjs';
+import { NhomNhaCungCap } from './../../entities/thiet-lap/nhom-nha-cung-cap';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '@environments/environment';
+import { BaseService } from '..';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NhomNhaCungCapService extends BaseService {
+  
+  private apiUrl: string = environment.apiUrl + '/nhomnhacungcap';
+
+  constructor (private httpClient: HttpClient){super();}
+
+  findNhomNhaCungCap(id: number): Observable<NhomNhaCungCap> {
+    return  this.httpClient.get<NhomNhaCungCap>(this.apiUrl + `/${id}`);
+  }
+
+  findNhomNhaCungCaps(noAuth: boolean =false): Observable<NhomNhaCungCap[]>{
+    if(noAuth){
+      var reqHeader = new HttpHeaders ({'Content-Type': 'application/x-www-urlencoded', 'No-Auth': 'True'});
+      return this.httpClient.get<any>(this.apiUrl + `/not-auth`, {headers: reqHeader});
+    }
+    return this.httpClient.get<NhomNhaCungCap[]>(this.apiUrl );
+  }
+
+  addNhomNhaCungCap(nhomnhacungcap: NhomNhaCungCap): Observable<NhomNhaCungCap>{
+    return this.httpClient.post<NhomNhaCungCap>(this.apiUrl ,nhomnhacungcap);
+  }
+
+  updateNhomNhaCungCap(nhomnhacungcap: NhomNhaCungCap): Observable<NhomNhaCungCap>{
+    return this.httpClient.put<NhomNhaCungCap>(this.apiUrl  + `/${nhomnhacungcap.id}`, nhomnhacungcap);
+  }
+
+  deleteNhomNhaCungCap(id: number): Observable<NhomNhaCungCap>{
+    return this.httpClient.delete<NhomNhaCungCap>(this.apiUrl + `/${id}`);
+  }
+
+  checkExistNhomNhaCungCap(manhomnhacungcap: string, manhomnhacungcap_old: string  =null){
+    if(manhomnhacungcap == manhomnhacungcap_old){
+      return new Promise((resolve) => {
+        setTimeout(function() {
+          resolve(true);
+        }, 300);
+      });
+    }
+    else{
+      return this.httpClient.get(this.apiUrl + `/exist?manhomnhacungcap=${manhomnhacungcap}`)
+      .toPromise()
+      .then(res => !res)
+      .catch(err =>{
+        console.error(err);
+        this.handleError(err);
+      });
+    }
+  }
+
+}
