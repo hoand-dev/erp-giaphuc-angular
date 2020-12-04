36c40267d3cbd90d1372f516ea1977de990b8df5
hoand-dev <hoand.dev@gmail.com>
2020-12-04 14:25:21 +0700

    tính lại tiền

--- src/app/modules/mua-hang/pages/phieu-tra-hang-ncc-cap-nhat/phieu-tra-hang-ncc-cap-nhat.component.ts
+++ src/app/modules/mua-hang/pages/phieu-tra-hang-ncc-cap-nhat/phieu-tra-hang-ncc-cap-nhat.component.ts
@@ -195,7 +195,7 @@ export class PhieuTraHangNCCCapNhatComponent implements OnInit {
         }
 
         // tính tổng tiền
-        this.onTinhTong();
+        this.onTinhTien();
     }
 
     public onHangHoaAdd() {
@@ -255,21 +255,18 @@ export class PhieuTraHangNCCCapNhatComponent implements OnInit {
                 break;
         }
 
-        this.hanghoas[index].thanhtien = this.hanghoas[index].soluong * this.hanghoas[index].dongia;
-        this.hanghoas[index].thanhtien =
-            this.hanghoas[index].thanhtien -
-            this.hanghoas[index].thanhtien * this.hanghoas[index].chietkhau +
-            (this.hanghoas[index].thanhtien - this.hanghoas[index].thanhtien * this.hanghoas[index].chietkhau) * this.hanghoas[index].thuevat;
-
-        // tính tổng tiền sau chiết khấu
-        this.onTinhTong();
+        // tính tiền sau chiết khấu
+        this.onTinhTien();
     }
 
-    // tính tổng tiền hàng và tổng thành tiền sau chiết khấu
-    private onTinhTong() {
+    // tính tiền sau chiết khấu và tổng
+    private onTinhTien() {
         let tongtienhang: number = 0;
 
         this.hanghoas.forEach((v, i) => {
+            v.thanhtien = v.soluong * v.dongia;
+            v.thanhtien = v.thanhtien - v.thanhtien * v.chietkhau + (v.thanhtien - v.thanhtien * v.chietkhau) * v.thuevat;
+
             tongtienhang += v.thanhtien;
         });
         this.phieutrahangncc.tongtienhang = tongtienhang;
