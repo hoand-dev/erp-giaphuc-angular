2793ffc768bdea974a186e5033b9155b270f3e97
hoand-dev <hoand.dev@gmail.com>
2020-12-04 14:24:50 +0700

    tattoan

--- src/app/modules/mua-hang/pages/phieu-mua-hang-ncc-cap-nhat/phieu-mua-hang-ncc-cap-nhat.component.ts
+++ src/app/modules/mua-hang/pages/phieu-mua-hang-ncc-cap-nhat/phieu-mua-hang-ncc-cap-nhat.component.ts
@@ -38,6 +38,9 @@ export class PhieuMuaHangNCCCapNhatComponent implements OnInit {
     // điều kiện để hiển thị danh sách hàng hoá
     public isValidForm: boolean = false;
 
+    // tất toán
+    public isTatToan: boolean = false;
+
     // dùng để kiểm tra load lần đầu (*)
     private hanghoalenght: number = 0;
 
@@ -123,6 +126,15 @@ export class PhieuMuaHangNCCCapNhatComponent implements OnInit {
                 }
             })
         );
+
+        // kiểm tra queryParams
+        this.subscriptions.add(
+            this.activatedRoute.queryParams.subscribe((params) => {
+                if (this.commonService.isNotEmpty(params.tattoan)) {
+                    this.isTatToan = true;
+                }
+            })
+        );
     }
 
     ngOnDestroy(): void {
@@ -168,7 +180,7 @@ export class PhieuMuaHangNCCCapNhatComponent implements OnInit {
         }
 
         // tính tổng tiền
-        this.onTinhTong();
+        this.onTinhTien();
     }
 
     public onHangHoaAdd() {
@@ -198,14 +210,17 @@ export class PhieuMuaHangNCCCapNhatComponent implements OnInit {
         }
 
         // chỉ thêm row mới khi không tồn tài dòng rỗng nào
-        let rowsNull = this.hanghoas.filter((x) => x.hanghoa_id == null);
-        if (rowsNull.length == 0) {
-            this.onHangHoaAdd();
-        }
+        // let rowsNull = this.hanghoas.filter((x) => x.hanghoa_id == null);
+        // if (rowsNull.length == 0) {
+        //     this.onHangHoaAdd();
+        // }
     }
 
     public onHangHoaChangeRow(col: string, index: number, e: any) {
         switch (col) {
+            case 'soluongtattoan':
+                this.hanghoas[index].soluongtattoan = e.value;
+                break;
             case 'soluong':
                 this.hanghoas[index].soluong = e.value;
                 break;
@@ -226,21 +241,17 @@ export class PhieuMuaHangNCCCapNhatComponent implements OnInit {
                 break;
         }
 
-        this.hanghoas[index].thanhtien = this.hanghoas[index].soluong * this.hanghoas[index].dongia;
-        this.hanghoas[index].thanhtien =
-            this.hanghoas[index].thanhtien -
-            this.hanghoas[index].thanhtien * this.hanghoas[index].chietkhau +
-            (this.hanghoas[index].thanhtien - this.hanghoas[index].thanhtien * this.hanghoas[index].chietkhau) * this.hanghoas[index].thuevat;
-
-        // tính tổng tiền sau chiết khấu
-        this.onTinhTong();
+        // tính tiền sau chiết khấu
+        this.onTinhTien();
     }
 
-    // tính tổng tiền hàng và tổng thành tiền sau chiết khấu
-    private onTinhTong() {
+    // tính tiền sau chiết khấu và tổng
+    private onTinhTien() {
         let tongtienhang: number = 0;
 
         this.hanghoas.forEach((v, i) => {
+            v.thanhtien = (v.soluong - v.soluongtattoan) * v.dongia;
+            v.thanhtien = v.thanhtien - v.thanhtien * v.chietkhau + (v.thanhtien - v.thanhtien * v.chietkhau) * v.thuevat;
             tongtienhang += v.thanhtien;
         });
         this.phieumuahangncc.tongtienhang = tongtienhang;
@@ -258,28 +269,53 @@ export class PhieuMuaHangNCCCapNhatComponent implements OnInit {
         phieumuahangncc_req.phieumuahangncc_chitiet = hanghoas;
 
         this.saveProcessing = true;
-        this.subscriptions.add(
-            this.phieumuahangnccService.updatePhieuMuaHangNCC(phieumuahangncc_req).subscribe(
-                (data) => {
-                    notify(
-                        {
-                            width: 320,
-                            message: 'Lưu thành công',
-                            position: { my: 'right top', at: 'right top' }
-                        },
-                        'success',
-                        475
-                    );
-                    this.router.navigate(['/phieu-mua-hang-ncc']);
-                    this.frmPhieuMuaHangNCC.instance.resetValues();
-                    this.saveProcessing = false;
-                },
-                (error) => {
-                    this.phieumuahangnccService.handleError(error);
-                    this.saveProcessing = false;
-                }
-            )
-        );
+
+        if(this.isTatToan)
+            this.subscriptions.add(
+                this.phieumuahangnccService.updateTatToanPhieuMuaHangNCC(phieumuahangncc_req).subscribe(
+                    (data) => {
+                        notify(
+                            {
+                                width: 320,
+                                message: 'Lưu thành công',
+                                position: { my: 'right top', at: 'right top' }
+                            },
+                            'success',
+                            475
+                        );
+                        this.router.navigate(['/phieu-mua-hang-ncc']);
+                        this.frmPhieuMuaHangNCC.instance.resetValues();
+                        this.saveProcessing = false;
+                    },
+                    (error) => {
+                        this.phieumuahangnccService.handleError(error);
+                        this.saveProcessing = false;
+                    }
+                )
+            );
+        else
+            this.subscriptions.add(
+                this.phieumuahangnccService.updatePhieuMuaHangNCC(phieumuahangncc_req).subscribe(
+                    (data) => {
+                        notify(
+                            {
+                                width: 320,
+                                message: 'Lưu thành công',
+                                position: { my: 'right top', at: 'right top' }
+                            },
+                            'success',
+                            475
+                        );
+                        this.router.navigate(['/phieu-mua-hang-ncc']);
+                        this.frmPhieuMuaHangNCC.instance.resetValues();
+                        this.saveProcessing = false;
+                    },
+                    (error) => {
+                        this.phieumuahangnccService.handleError(error);
+                        this.saveProcessing = false;
+                    }
+                )
+            );
         e.preventDefault();
     }
 }
