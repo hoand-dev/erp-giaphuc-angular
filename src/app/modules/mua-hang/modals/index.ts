bb689ea9d3ce390d697f386ebff68c8b46c256a6
hoand-dev <hoand.dev@gmail.com>
2021-01-29 15:13:35 +0700

    update path index mua hang

---
+++ src/app/modules/mua-hang/modals/index.ts
@@ -0,0 +1,4 @@
+export * from './danh-sach-phieu-dat-hang-ncc-modal/danh-sach-phieu-dat-hang-ncc-modal.component';
+export * from './danh-sach-phieu-mua-hang-ncc-modal/danh-sach-phieu-mua-hang-ncc-modal.component';
+export * from './danh-sach-phieu-tra-hang-ncc-modal/danh-sach-phieu-tra-hang-ncc-modal.component';
+export * from './phieu-dat-hang-ncc-in-phieu-modal/phieu-dat-hang-ncc-in-phieu-modal.component';
