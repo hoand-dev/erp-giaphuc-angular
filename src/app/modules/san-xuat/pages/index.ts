450be28252ebb03a32366b5ba3a44070313af824
hoand-dev <hoand.dev@gmail.com>
2021-01-29 15:25:47 +0700

    update path index san xuat

---
+++ src/app/modules/san-xuat/pages/index.ts
@@ -0,0 +1,20 @@
+export * from './bang-gia-gia-cong/bang-gia-gia-cong.component';
+export * from './bang-gia-gia-cong-cap-nhat/bang-gia-gia-cong-cap-nhat.component';
+export * from './bang-gia-gia-cong-them-moi/bang-gia-gia-cong-them-moi.component';
+
+export * from './phieu-cap-phat-vat-tu/phieu-cap-phat-vat-tu.component';
+export * from './phieu-cap-phat-vat-tu-cap-nhat/phieu-cap-phat-vat-tu-cap-nhat.component';
+export * from './phieu-cap-phat-vat-tu-them-moi/phieu-cap-phat-vat-tu-them-moi.component';
+
+export * from './phieu-nhap-kho-gia-cong/phieu-nhap-kho-gia-cong.component';
+export * from './phieu-nhap-kho-gia-cong-cap-nhat/phieu-nhap-kho-gia-cong-cap-nhat.component';
+export * from './phieu-nhap-kho-gia-cong-them-moi/phieu-nhap-kho-gia-cong-them-moi.component';
+
+export * from './phieu-xuat-kho-gia-cong/phieu-xuat-kho-gia-cong.component';
+export * from './phieu-xuat-kho-gia-cong-cap-nhat/phieu-xuat-kho-gia-cong-cap-nhat.component';
+export * from './phieu-xuat-kho-gia-cong-them-moi/phieu-xuat-kho-gia-cong-them-moi.component';
+
+export * from './phieu-yeu-cau-gia-cong/phieu-yeu-cau-gia-cong.component';
+export * from './phieu-yeu-cau-gia-cong-cap-nhat/phieu-yeu-cau-gia-cong-cap-nhat.component';
+export * from './phieu-yeu-cau-gia-cong-them-moi/phieu-yeu-cau-gia-cong-them-moi.component';
+
