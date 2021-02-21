44d39264fafe8e3a03982498d47023d3ab714fd0
hoand-dev <hoand.dev@gmail.com>
2021-02-21 14:41:48 +0700

    add trang thai giao

--- src/app/shared/enums/e-trang-thai-phieu.enum.ts
+++ src/app/shared/enums/e-trang-thai-phieu.enum.ts
@@ -1,34 +1,37 @@
 export enum ETrangThaiPhieu {
-    chuanhan = "Chưa nhận",
-    dangnhan = "Đang nhận",
-    danhan = "Đã nhận",
+    chuanhan   = "Chưa nhận"  ,
+    dangnhan   = "Đang nhận"  ,
+    danhan     = "Đã nhận"    ,
 
-    chuanhap = "Chưa nhập",
-    dangnhap = "Đang nhập",
-    danhap = "Đã nhập",
+    chuanhap   = "Chưa nhập"  ,
+    dangnhap   = "Đang nhập"  ,
+    danhap     = "Đã nhập"    ,
 
-    chuaxuat = "Chưa xuất",
-    dangxuat = "Đang xuất",
-    daxuat = "Đã xuất",
+    chuaxuat   = "Chưa xuất"  ,
+    dangxuat   = "Đang xuất"  ,
+    daxuat     = "Đã xuất"    ,
 
     chuachuyen = "Chưa chuyển",
     dangchuyen = "Đang chuyển",
-    dachuyen = "Đã chuyển",
+    dachuyen   = "Đã chuyển"  ,
 
-    chuathu = "Chưa thu",
-    dangthu = "Đang thu",
-    dathu = "Đã thu",
+    chuathu    = "Chưa thu"   ,
+    dangthu    = "Đang thu"   ,
+    dathu      = "Đã thu"     ,
 
-    chuachi = "Chưa chi",
-    dangchi = "Đang chi",
-    dachi = "Đã chi",
+    chuachi    = "Chưa chi"   ,
+    dangchi    = "Đang chi"   ,
+    dachi      = "Đã chi"     ,
 
-    choduyet = "Chờ duyệt",
-    daduyet = "Đã duyệt",
+    choduyet   = "Chờ duyệt"  ,
+    daduyet    = "Đã duyệt"   ,
+    dahuy      = "Đã huỷ"     ,
 
-    dahuy = "Đã huỷ",
-
-    chuatra = "Chưa trả",
-    dangtra = "Đang trả",
-    datra   = "Đã trả"
+    chuatra    = "Chưa trả"   ,
+    dangtra    = "Đang trả"   ,
+    datra      = "Đã trả"     ,
+    
+    chuagiao   = "Chưa giao"  ,
+    danggiao   = "Đang giao"  ,
+    dagiao     = "Đã giao"
 }
