ca7ad6c92475cbf2b245aa80adc15d7f67d316e2
hoand-dev <hoand.dev@gmail.com>
2021-04-10 09:07:13 +0700

    bổ sung các trường: đơn giá, thành tiền, thuế, ck, chú thích .v.v..

--- src/app/shared/entities/thong-ke/ban-hang/thong-ke-xuat-ban-chi-tiet.ts
+++ src/app/shared/entities/thong-ke/ban-hang/thong-ke-xuat-ban-chi-tiet.ts
@@ -1,13 +1,20 @@
 export class ThongKeXuatBanChiTiet{
-    public ngayphatsinh      : string;
-    public maphieu           : string;
-    public makhoxuat         : string;
-    public khoxuat           : string;
-    public makhachhang       : string;
-    public tenkhachhang      : string;
-    public nhanviensale      : string;
-    public mahanghoa         : string;
-    public tenhanghoa        : string;
-    public tenhanghoa_inphieu: string;
-    public soluong           : number;
+    public ngayphatsinh       : string;
+    public maphieu            : string;
+    public makhoxuat          : string;
+    public khoxuat            : string;
+    public makhachhang        : string;
+    public tenkhachhang       : string;
+    public nhanviensale       : string;
+    public mahanghoa          : string;
+    public tenhanghoa         : string;
+    public tenhanghoa_inphieu : string;
+    public soluong            : number;
+    public dongia             : number;
+    public thanhtien          : number;
+    public chietkhau          : number;
+    public thuevat            : number;
+    public dongiavat          : number;
+    public thanhtienvat       : number;
+    public chuthich           : string;
 }
