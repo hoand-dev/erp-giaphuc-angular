b1bf6caa506f6b82e1a02e96befdfc41139f5adc
LarryTeo <44776310+LarryTeo@users.noreply.github.com>
2021-04-20 10:30:24 +0700

    Add_DieuChinhKho_View_Modal

--- src/app/modules/kho-hang/kho-hang.module.ts
+++ src/app/modules/kho-hang/kho-hang.module.ts
@@ -49,6 +49,13 @@ import {
  } from './modals';
 import { PhieuDieuChinhKhoInPhieuModalComponent } from './modals/phieu-dieu-chinh-kho-in-phieu-modal/phieu-dieu-chinh-kho-in-phieu-modal.component';
 import { PhieuNhapKhoViewModalComponent } from './modals/phieu-nhap-kho-view-modal/phieu-nhap-kho-view-modal.component';
+import { PhieuDieuChinhKhoViewModalComponent } from './modals/phieu-dieu-chinh-kho-view-modal/phieu-dieu-chinh-kho-view-modal.component';
+import { PhieuXuatChuyenKhoViewModalComponent } from './modals/phieu-xuat-chuyen-kho-view-modal/phieu-xuat-chuyen-kho-view-modal.component';
+import { PhieuNhapChuyenKhoViewModalComponent } from './modals/phieu-nhap-chuyen-kho-view-modal/phieu-nhap-chuyen-kho-view-modal.component';
+import { PhieuXuatMuonViewModalComponent } from './modals/phieu-xuat-muon-view-modal/phieu-xuat-muon-view-modal.component';
+import { PhieuNhapTraViewModalComponent } from './modals/phieu-nhap-tra-view-modal/phieu-nhap-tra-view-modal.component';
+import { PhieuNhapMuonViewModalComponent } from './modals/phieu-nhap-muon-view-modal/phieu-nhap-muon-view-modal.component';
+import { PhieuXuatTraViewModalComponent } from './modals/phieu-xuat-tra-view-modal/phieu-xuat-tra-view-modal.component';
 
 @NgModule({
     declarations: [
@@ -91,7 +98,14 @@ import { PhieuNhapKhoViewModalComponent } from './modals/phieu-nhap-kho-view-mod
         PhieuNhapTraMuonHangInPhieuModalComponent,
         PhieuXuatTraMuonHangInPhieuModalComponent,
         PhieuDieuChinhKhoInPhieuModalComponent,
-        PhieuNhapKhoViewModalComponent
+        PhieuNhapKhoViewModalComponent,
+        PhieuDieuChinhKhoViewModalComponent,
+        PhieuXuatChuyenKhoViewModalComponent,
+        PhieuNhapChuyenKhoViewModalComponent,
+        PhieuXuatMuonViewModalComponent,
+        PhieuNhapTraViewModalComponent,
+        PhieuNhapMuonViewModalComponent,
+        PhieuXuatTraViewModalComponent
     ],
     imports: [CommonModule, SharedModule, KhoHangRoutingModule]
 })
